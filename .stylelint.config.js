33881f78230aa41141777687685d7a8f5eb39a98
manal <manalz1619@gmail.com>
2024-09-20 22:54:58 +0500

    checked css linters

--- .stylelint.config.js
+++ .stylelint.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-extends: "stylelint-config-standard",
-rules : {
-"indentation" : 2,
-"no-empty-source" : null,
-},
-}
+    extends: "stylelint-config-standard",
+    rules: {
+      "color-hex-case": "lower",
+      "indentation": 4,
+      "max-empty-lines": 1,
+      "block-closing-brace-newline-before": "always",
+      "no-eol-whitespace": true,
+      "declaration-block-no-duplicate-properties": true,
+    },
+  };
 
